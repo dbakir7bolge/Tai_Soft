d5a41e9fe81b59211b2648cd37aa4cca1d281dc5
dbakir7bolge <dbakir7bolge@gmail.com>
2026-08-25 01:05:28 +0300

    Delete BILGISAYARDA_TEST_ET.bat

--- BILGISAYARDA_TEST_ET.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-where python >nul 2>nul || (echo Python bulunamadi. index.html dosyasini acabilirsiniz, ancak PWA testleri icin HTTPS/localhost gerekir.& pause & exit /b 1)
-start "" http://127.0.0.1:8080
-python -m http.server 8080
